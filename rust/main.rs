9c3dc87040a4b52fa6045fe9add42e1d4fd3ed40
lbrande <lovbra00@gmail.com>
2020-08-22 22:51:35 +0200

    Removed unnecessary imports, added imports for fast output.

--- rust/main.rs
+++ rust/main.rs
@@ -1,17 +1,16 @@
 #![allow(unused_imports)]
 #![allow(unused_macros)]
-use std::{
-    cmp::{max, min},
-    collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
-    io::{self, stdin, BufRead, Read, Write},
-};
 
 #[macro_use]
 mod scanner;
-use self::scanner::*;
+
+use std::{
+    cmp::{max, min},
+    collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
+    io::{stdout, BufWriter, Write},
+};
 
 fn main() {
     let n = scan!(usize);
     let xs = scan!(usize; n);
 }
-
